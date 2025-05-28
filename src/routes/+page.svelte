1be95c57b33802ada7e1ecaefb301f81788d009b
manaphy <nenoi22@c.kyoai.ac.jp>
2025-05-28 11:46:14 +0900

    sinplestmap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<div class="flex justify-center text-6xl text-red-500">wataridori-map</div>
+<script lang="ts">
+  import { MapLibre, Marker } from 'svelte-maplibre-gl';
+</script>
+
+<MapLibre
+  zoom={5}
+  center={[142, 43]}
+  class="h-[400px]"
+  style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+>
+  <Marker lnglat={[141.692222, 42.775]} />
+</MapLibre>

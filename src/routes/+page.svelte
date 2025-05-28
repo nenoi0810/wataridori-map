451df6c50192737afbfd9cdbf3afa256bcc28ed2
manaphy <nenoi22@c.kyoai.ac.jp>
2025-05-28 23:49:26 +0900

    マップのUIを改善

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
 	import { CircleLayer, GeoJSONSource, MapLibre } from 'svelte-maplibre-gl';
+	import type { GeoJSON } from 'geojson';
 	import observasion from '$lib/observation.json';
 
 	let sliderValue = $state(5);
 	let selectedOption = $state('オオハクチョウ');
 </script>
 
-<div class="flex">
-	<div>
-		{sliderValue}月:
-		<input type="range" min="1" max="12" bind:value={sliderValue} class="slider" id="myRange" />
-	</div>
-	<select bind:value={selectedOption} class="select">
-		<option value="オオハクチョウ">オオハクチョウ</option>
-		<option value="コハクチョウ">コハクチョウ</option>
-		<option value="カモ類">カモ類</option>
-	</select>
-</div>
-
 <MapLibre
-	zoom={5}
-	center={[142, 43]}
-	class="h-[400px]"
+	zoom={4}
+	center={[137.5, 37.5]}
+	class="h-[800px] w-full"
 	style={{
 		version: 8,
 		sources: {
@@ -42,7 +31,7 @@
 		]
 	}}
 >
-	<GeoJSONSource data={observasion}>
+	<GeoJSONSource data={observasion as GeoJSON}>
 		<CircleLayer
 			id="observations"
 			paint={{
@@ -52,3 +41,18 @@
 		/>
 	</GeoJSONSource>
 </MapLibre>
+
+<div class="flex justify-center gap-8 mt-4 items-center">
+	<div class="flex items-center gap-2">
+		<span>{sliderValue}月</span>
+		<input type="range" min="1" max="12" bind:value={sliderValue} class="slider w-[200px]" id="myRange" />
+	</div>
+	<div class="flex items-center gap-2">
+		<span>鳥の種類：</span>
+		<select bind:value={selectedOption} class="select p-1 border rounded w-[150px]">
+			<option value="オオハクチョウ">オオハクチョウ</option>
+			<option value="コハクチョウ">コハクチョウ</option>
+			<option value="カモ類">カモ類</option>
+		</select>
+	</div>
+</div>

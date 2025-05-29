b30b6ff38ed7c8c0ab5fa3ddcdd5302f4609537f
Kanahiro <Kanahiro@users.noreply.github.com>
2025-05-29 01:49:52 +0000

    deploy: cff0402457064fd3824600cd838ee5855af9c034

--- _app/immutable/nodes/0.CFTT4X3G.js
+++
@@ -1,1 +0,0 @@
-import{c as n,a}from"../chunks/C5E_X2tp.js";import{i}from"../chunks/BWvJKPR5.js";import{s as p}from"../chunks/DkMXsSP8.js";const s=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:s},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=i(e);p(t,()=>r.children),a(o,e)}export{u as component,f as universal};

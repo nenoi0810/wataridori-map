a740e293557fe9e632bddd047f1bc28fe9197ea3
Kanahiro <Kanahiro@users.noreply.github.com>
2025-05-29 08:15:57 +0000

    deploy: 706fedb453357acbee177db1c8e29afb534317e4

--- _app/immutable/nodes/0.843EQMDo.js
+++
@@ -1,1 +0,0 @@
-import{c as n,a}from"../chunks/yhCB6LYV.js";import{i}from"../chunks/D4SX5mVv.js";import{s as p}from"../chunks/CKMupnwZ.js";const s=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:s},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=i(e);p(t,()=>r.children),a(o,e)}export{u as component,f as universal};
